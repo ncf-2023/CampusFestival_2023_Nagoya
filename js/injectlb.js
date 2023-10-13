1f8f10816fae50b8052906583997d55f0f41d392
aika-n-toki <kotetsu_21n1100600@nnn.ed.jp>
2023-10-13 23:45:23 +0900

    about.html軽量化

--- js/injectlb.js
+++ js/injectlb.js
@@ -4,6 +4,7 @@ function setLightboxOne(q) {
     new SimpleLightbox(q, {
       sourceAttr: "data-raw",
       nav: false,
+      captions: true,
       captionsData: "alt",
       animationSlide: false,
       loop: false,
@@ -15,11 +16,8 @@ function setLightboxMany(q) {
   lba.push(
     new SimpleLightbox(q, {
       sourceAttr: "data-raw",
-      navText: [
-        '<img style="width:inherit;" src="../media/chevron_left.svg">',
-        '<img style="width:inherit;" src="../media/chevron_right.svg">',
-      ],
-      captionsData: "alt",
+      navText: ['<img style="width:inherit;" src="../media/chevron_left.svg">', '<img style="width:inherit;" src="../media/chevron_right.svg">'],
+      // captionsData: "alt",
       overlayOpacity: 0.8,
     })
   );
