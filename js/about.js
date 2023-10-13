1ff3acd0ae4b2ac7c9c9562f92ee45a8659da7eb
aika-n-toki <kotetsu_21n1100600@nnn.ed.jp>
2023-10-14 02:36:09 +0900

    about.html 背景追加

--- js/about.js
+++ js/about.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", (_) => {
           // console.log(e);
           let cn = document.querySelector("#posterTemplate").cloneNode(true);
           cn.querySelector("img").src = "./media/" + e.f.substring(0, e.f.length - 4) + "-thumb" + e.f.substring(e.f.length - 4, e.f.length);
-          cn.querySelector("img").setAttribute("data-raw", "./media/" + e.f);
-          cn.querySelector("img").setAttribute("title", e.t + "\\n" + e.d);
+          cn.querySelector("a").setAttribute("href", "./media/" + e.f);
+          cn.querySelector("img").setAttribute("title", e.t + " ─ " + (e.a || "匿名希望") + '<br class="pc sp">' + e.d);
           cn.querySelector("h4").textContent = e.t;
           cn.querySelector("h6").textContent = e.a || "匿名希望";
           cn.querySelector("h6").classList.add(e.d ? "mb-3" : "mb-0");
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", (_) => {
           // console.log(e);
           let cn = document.querySelector("#characterTemplate").cloneNode(true);
           cn.querySelector("img").src = "./media/" + e.f.substring(0, e.f.length - 4) + "-thumb" + e.f.substring(e.f.length - 4, e.f.length);
-          cn.querySelector("img").setAttribute("data-raw", "./media/" + e.f);
-          cn.querySelector("img").setAttribute("title", e.t + "\\n" + e.d);
+          cn.querySelector("a").setAttribute("href", "./media/" + e.f);
+          cn.querySelector("img").setAttribute("title", e.t + " ─ " + (e.a || "匿名希望") + '<br class="pc sp">' + e.d);
           cn.querySelector("h4").textContent = e.t;
           cn.querySelector("h6").textContent = e.a || "匿名希望";
           cn.querySelector("h6").classList.add(e.d ? "mb-3" : "mb-0");
@@ -48,8 +48,8 @@ document.addEventListener("DOMContentLoaded", (_) => {
           // console.log(e);
           let cn = document.querySelector("#illustTemplate").cloneNode(true);
           cn.querySelector("img").src = "./media/" + e.f.substring(0, e.f.length - 4) + "-thumb" + e.f.substring(e.f.length - 4, e.f.length);
-          cn.querySelector("img").setAttribute("data-raw", "./media/" + e.f);
-          cn.querySelector("img").setAttribute("title", e.t + "\\n" + e.d);
+          cn.querySelector("a").setAttribute("href", "./media/" + e.f);
+          cn.querySelector("img").setAttribute("title", e.t + " ─ " + (e.a || "匿名希望") + '<br class="pc sp">' + e.d);
           cn.querySelector("h4").textContent = e.t;
           cn.querySelector("h6").textContent = e.a || "匿名希望";
           cn.querySelector("h6").classList.add(e.d ? "mb-3" : "mb-0");
@@ -64,17 +64,44 @@ document.addEventListener("DOMContentLoaded", (_) => {
       setLightboxMany(".zoomWorksCharacter");
       setLightboxMany(".zoomWorksIllust");
       setLightboxMany(".zoomGoods");
-      setScrollbar("#content");
-      setScrollbar(".accordion-body");
+      setScrollbar("#content", (_) => runScroll());
+      setScrollbar(".accordion-body", false);
+      fixScrollbar();
+      setScrollSpeed("#backgroundBase", 1);
+      setScrollSpeed("#backgroundShapesCircle", 1.4);
+      setScrollSpeed("#backgroundShapesLine", 1.6);
+      setScrollSpeed("#backgroundShapesQuadrilateral", 1.8);
+      setScrollSpeed("#backgroundShapesTriangle", 2);
     });
 });
-function setScrollbar(e) {
+function setScrollbar(e, c) {
   $(e).mCustomScrollbar({
     axis: "y",
     theme: "rounded-dots-dark",
     scrollbarPosition: "inside",
-    scrollInertia: 180,
+    scrollInertia: 350,
     mouseWheel: { scrollAmount: 150 },
-    scrollButtons: { enable: true },
+    scrollButtons: { enable: false },
+    callbacks: {
+      whileScrolling: c,
+    },
+  });
+}
+function fixScrollbar() {
+  document.querySelectorAll(".mCSB_dragger").forEach((e) => {
+    let el = e.querySelector(".mCSB_draggerRail");
+    e.after(el);
+  });
+}
+let scrollContent = [];
+function runScroll() {
+  scrollContent.forEach((e) => e());
+}
+function setScrollSpeed(e, t) {
+  scrollContent.push((_) => {
+    let root = document.querySelector("#content > div > .mCSB_container");
+    let rootScrollAmount = Number(root.style.top.substring(0, root.style.top.length - 2));
+    document.querySelector(e).style.top = String(rootScrollAmount * t) + "px";
+    document.querySelector(e).style.height = String(root.clientHeight * t) + "px";
   });
 }
--- js/injectlb.js
+++ js/injectlb.js
@@ -2,10 +2,8 @@ let lba = [];
 function setLightboxOne(q) {
   lba.push(
     new SimpleLightbox(q, {
-      sourceAttr: "data-raw",
+      sourceAttr: "href",
       nav: false,
-      captions: true,
-      captionsData: "alt",
       animationSlide: false,
       loop: false,
       overlayOpacity: 0.8,
@@ -15,7 +13,7 @@ function setLightboxOne(q) {
 function setLightboxMany(q) {
   lba.push(
     new SimpleLightbox(q, {
-      sourceAttr: "data-raw",
+      sourceAttr: "href",
       navText: ['<img style="width:inherit;" src="../media/chevron_left.svg">', '<img style="width:inherit;" src="../media/chevron_right.svg">'],
       // captionsData: "alt",
       overlayOpacity: 0.8,
